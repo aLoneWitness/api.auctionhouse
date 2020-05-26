d7511e396b158c78543b28b36df47da611161cec
Sander van Dinteren <sanvandinteren@gmail.com>
2020-05-26 10:52:31 +0200

    Added JSON rules to Dto's to prevent recursive pattern when having a backreference.

--- src/main/java/auctionhouse/dto/ItemDto.java
+++ src/main/java/auctionhouse/dto/ItemDto.java
@@ -1,11 +1,17 @@
 package auctionhouse.dto;
 
 import auctionhouse.entities.User;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.StringIdGenerator.class,
+        property="id")
 public class ItemDto {
     @Getter @Setter
     private Integer id;
@@ -22,6 +28,7 @@ public class ItemDto {
     @Getter @Setter
     private String description;
 
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     @Getter @Setter
     private UserDto seller;
 }
--- src/main/java/auctionhouse/dto/UserDto.java
+++ src/main/java/auctionhouse/dto/UserDto.java
@@ -1,18 +1,28 @@
 package auctionhouse.dto;
 
 import auctionhouse.entities.Item;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.StringIdGenerator.class,
+        property="id")
 public class UserDto {
+    @Getter @Setter
+    private Integer id;
+
     @Getter @Setter
     private String username;
 
     @Getter @Setter
     private String email;
 
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     @Getter @Setter
-    private List<Item> inventory;
+    private List<ItemDto> inventory;
 }
